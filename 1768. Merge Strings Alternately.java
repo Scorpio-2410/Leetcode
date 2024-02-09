132193cc4f984860e4ab276d1c154b4d72f13228
Nabidul Islam <Nabidulislam90@gmail.com>
2024-02-09 12:06:41 +1100

    Updated comment layout

--- 1768. Merge Strings Alternately.java
+++ 1768. Merge Strings Alternately.java
@@ -1,14 +1,13 @@
 /* 1768. Merge Strings Alternately
- * 
- * You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
- * Return the merged string.
- * 
- * Input: word1 = "abc", word2 = "pqr"
- * Output: "apbqcr"
- * Explanation: The merged string will be merged as so:
- * word1:  a   b   c
- * word2:    p   q   r
- * merged: a p b q c r
+ You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+ Return the merged string.
+ 
+ Input: word1 = "abc", word2 = "pqr"
+ Output: "apbqcr"
+ Explanation: The merged string will be merged as so:
+ word1:  a   b   c
+ word2:    p   q   r
+ merged: a p b q c r
  */
 
 class Solution {
